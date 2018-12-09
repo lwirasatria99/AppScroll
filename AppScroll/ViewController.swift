d4a0ae5c104567661bd7263a43b7e5ce16b6100b
wira.satria90 <wira.satria90@gmail.com>
2018-12-09 10:53:01 +0700

    First commit

--- AppScroll/ViewController.swift
+++ AppScroll/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  AppScroll
 //
-//  Created by Elabram on 06/12/18.
+//  Created by L Wira on 06/12/18.
 //  Copyright © 2018 Elabram. All rights reserved.
 //
 
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //let viewHeight: CGFloat = 0.0
+        //view2.visiblity(gone: true, dimension: 0)
+        // or viewHeight = yourViewHeight
     }
 
+}
 
+extension UIView {
+    func visiblity(gone: Bool, dimension: CGFloat = 0.0, attribute: NSLayoutConstraint.Attribute = .height) -> Void {
+        if let constraint = (self.constraints.filter{$0.firstAttribute == attribute}.first) {
+            constraint.constant = gone ? 0.0 : dimension
+            self.layoutIfNeeded()
+            self.isHidden = gone
+        }
+    }
 }
 
